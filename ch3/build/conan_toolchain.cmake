7614489e0c0ca393306ce92c81d929ca8ddf4bc6
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-08-06 23:25:47 +0900

    [feat](ch3): link catch2

--- ch3/build/conan_toolchain.cmake
+++ ch3/build/conan_toolchain.cmake
@@ -31,7 +31,7 @@ cmake_policy(GET CMP0091 POLICY_CMP0091)
 if(NOT "${POLICY_CMP0091}" STREQUAL NEW)
     message(FATAL_ERROR "The CMake policy CMP0091 must be NEW, but is '${POLICY_CMP0091}'")
 endif()
-set(CMAKE_MSVC_RUNTIME_LIBRARY "$<$<CONFIG:Release>:MultiThreaded>")
+set(CMAKE_MSVC_RUNTIME_LIBRARY "$<$<CONFIG:Debug>:MultiThreadedDebug>")
 
 string(APPEND CONAN_CXX_FLAGS " /MP16")
 string(APPEND CONAN_C_FLAGS " /MP16")
@@ -61,17 +61,17 @@ endif()
 set(CMAKE_FIND_PACKAGE_PREFER_CONFIG ON)
 
 # Definition of CMAKE_MODULE_PATH
-list(PREPEND CMAKE_MODULE_PATH "C:/Users/USER/.conan2/p/b/catch19e7b1bb5e983/p/lib/cmake/Catch2")
+list(PREPEND CMAKE_MODULE_PATH "C:/Users/USER/.conan2/p/b/catch8ba1918650385/p/lib/cmake/Catch2")
 # the generators folder (where conan generates files, like this toolchain)
 list(PREPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
 # Definition of CMAKE_PREFIX_PATH, CMAKE_XXXXX_PATH
 # The explicitly defined "builddirs" of "host" context dependencies must be in PREFIX_PATH
-list(PREPEND CMAKE_PREFIX_PATH "C:/Users/USER/.conan2/p/b/catch19e7b1bb5e983/p/lib/cmake/Catch2")
+list(PREPEND CMAKE_PREFIX_PATH "C:/Users/USER/.conan2/p/b/catch8ba1918650385/p/lib/cmake/Catch2")
 # The Conan local "generators" folder, where this toolchain is saved.
 list(PREPEND CMAKE_PREFIX_PATH ${CMAKE_CURRENT_LIST_DIR} )
-list(PREPEND CMAKE_LIBRARY_PATH "C:/Users/USER/.conan2/p/b/catch19e7b1bb5e983/p/lib")
-list(PREPEND CMAKE_INCLUDE_PATH "C:/Users/USER/.conan2/p/b/catch19e7b1bb5e983/p/include")
+list(PREPEND CMAKE_LIBRARY_PATH "C:/Users/USER/.conan2/p/b/catch8ba1918650385/p/lib")
+list(PREPEND CMAKE_INCLUDE_PATH "C:/Users/USER/.conan2/p/b/catch8ba1918650385/p/include")
 
 
 
---
+++ ch3/catchtest.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <catch2/catch_totals.hpp>
+
+int main()
+{
+
+}
